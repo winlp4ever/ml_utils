1a96553ffa7569c0c23820ebfc3d50afde40b114
ha quang le <winlp4ever@gmail.com>
2019-04-01 00:25:16 +0200

    rename to download_utils.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-from urllib.request import urlretrieve
-import sys, os, time
+from keras.layers import Conv2D, Dense
+from keras.models import Sequential
+import numpy as np
 
-for i in range(10000, 0, -1):
-    print(i, end='\r')
-    time.sleep(0.3)
-    sys.stdout.flush()
+model = Sequential([
+        Dense(2, input_shape=(3,), weights=[np.array([[0, 0], [0, 1], [1, 0]])], use_bias=False)
+    ])
+
+print(model.layers[0].get_weights())
